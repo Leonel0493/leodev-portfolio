1d440c59935a653750cc48e5e96c27fbd2e6ce9f
Leonel Rivas <leonrivas04@gmail.com>
2025-04-10 09:54:14 -0600

    HotFix: FC issue
    Remove FC call

--- src/components/Contact/Contact-Wrapper-Paragraph.tsx
+++ src/components/Contact/Contact-Wrapper-Paragraph.tsx
@@ -4,7 +4,7 @@ type ParagraphProps = {
   children: ReactNode;
 };
 
-const ContactWrapperParagraph: FC<ParagraphProps> = ({ children }) => {
+const ContactWrapperParagraph = ({ children }: ParagraphProps) => {
   return (
     <p className="font-JetBrains-Mono text-[#FFE7DE] text-sm font-light leading-[1.9] my-2.5">
       {children}
