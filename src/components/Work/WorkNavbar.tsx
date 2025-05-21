947246013dca5105fe94ac4169c5597275f1ecd9
Leonel Rivas <leonrivas04@gmail.com>
2025-05-21 14:04:44 -0600

    fix: Update layout of left menu

--- src/components/Work/WorkNavbar.tsx
+++ src/components/Work/WorkNavbar.tsx
@@ -4,27 +4,34 @@ import NavbarItem from "../Shared/NavbarItem";
 const WorkNavbar = () => {
   return (
     <div
-      id="navbar"
+      id="about-navbar-left"
       className="relative w-screen h-[10vh] px-[10%] flex flex-row justify-start items-center 
              sm:h-auto 
-             md:w-[10%] md:py-[30px] md:h-screen md:flex-col md:px-0 md:justify-start md:fixed
-             lg:w-[6%] lg:h-screen lg:top-0 lg:left-0 lg:z-50 lg:py-[15px]"
+             md:w-[10%] md:py-[20px] md:h-screen md:flex-col md:px-0 md:justify-start md:fixed
+            lg:w-[6%] lg:h-screen lg:top-0 lg:left-0 lg:z-50 lg:py-[15px]"
     >
-      <NavbarItem
-        href="/"
-        title="Home"
-        hoverColor="#3A94C5"
-        textColor="#FFE7DE"
-        isExternal={false}
-      />
-
-      {/* line separator */}
       <div
-        className="bg-[#FFE7DE] w-[50px] h-[1px] 
-                    md:w-[1px] md:h-[110px] md:my-6"
-      ></div>
+        id="navbar-items"
+        className="w-auto h-full flex flex-row justify-center items-center 
+        sm:py-[2.5%] 
+        md:h-auto md:flex-col md:py-[0] md:pt-[20%] md:justify-start"
+      >
+        <NavbarItem
+          href="/"
+          title="Home"
+          hoverColor="#3A94C5"
+          textColor="#FFE7DE"
+          isExternal={false}
+        />
 
-      <NavbarCopyright textColor="#FFE7DE" />
+        {/* line separator */}
+        <div
+          className="bg-[#FFE7DE] w-[50px] h-[1px] 
+                    md:w-[1px] md:h-[110px] md:my-6"
+        ></div>
+
+        <NavbarCopyright textColor="#FFE7DE" />
+      </div>
     </div>
   );
 };
